2a7fd34283e4701d32ea776df3093e093fe1b56a
Krieg, Alexander <alexander.krieg@ionos.com>
2022-07-30 10:15:20 +0200

    Add sync with cluster at boot time

--- cmd/main.go
+++ cmd/main.go
@@ -21,23 +21,25 @@ Configuration:     |___/
 var logger *utils.Logger
 
 type Configuration struct {
-	Verbose             bool   `default:"true"`
-	AdminPort           int    `default:"8081"`
-	MockPort            int    `default:"8080"`
-	MockDir             string `default:"."`
-	MockFilepattern     string `default:"*-mock.*"`
-	ResponseDir         string `default:"./responses"`
-	ResponseFilepattern string `default:"*.*"`
+	Verbose             bool     `default:"true"`
+	ConfigPort          int      `default:"8081" split_words:"true"`
+	MockPort            int      `default:"8080" split_words:"true"`
+	MockDir             string   `default:"." split_words:"true"`
+	MockFilepattern     string   `default:"*-mock.*" split_words:"true"`
+	ResponseDir         string   `default:"./responses" split_words:"true"`
+	ResponseFilepattern string   `default:"*.*" split_words:"true"`
+	ClusterUrls         []string `default:"" split_words:"true"`
 }
 
 func (c Configuration) info() string {
 	return fmt.Sprintf(`Verbose: %v
-Admin Port: %v
+Config Port: %v
 Mock Port: %v
 Mock Dir: '%s'
 Mock Filepattern: '%s'
 Response Dir: '%s'
-Response Filepattern: '%s'`, c.Verbose, c.AdminPort, c.MockPort, c.MockDir, c.MockFilepattern, c.ResponseDir, c.ResponseFilepattern)
+Response Filepattern: '%s'
+Cluster URLs: '%v'`, c.Verbose, c.ConfigPort, c.MockPort, c.MockDir, c.MockFilepattern, c.ResponseDir, c.ResponseFilepattern, c.ClusterUrls)
 }
 
 func main() {
@@ -51,9 +53,16 @@ func main() {
 
 	mockRouter, err := routing.NewMockRouter(config.MockDir, config.MockFilepattern, config.ResponseDir, config.ResponseFilepattern, logger)
 	if err != nil {
-		log.Fatalf("Can't load files: %v", err)
+		log.Fatalf("(FATAL) Can't load files: %v", err)
 	}
 
-	go routing.NewConfigRouter(mockRouter, logger).ListenAndServe(config.AdminPort)
+	configRouter := routing.NewConfigRouter(mockRouter, logger)
+	err = configRouter.SyncWithCluster(config.ClusterUrls)
+	if err != nil {
+		log.Fatalf("(FATAL) Can't sync with cluster: %v\n", err)
+	}
+
+	go configRouter.ListenAndServe(config.ConfigPort)
+
 	mockRouter.ListenAndServe(config.MockPort)
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require (
 	github.com/Masterminds/sprig v2.22.0+incompatible
 	github.com/go-http-utils/headers v0.0.0-20181008091004-fed159eddc2a
+	github.com/google/uuid v1.3.0
 	github.com/gorilla/mux v1.8.0
 	github.com/kelseyhightower/envconfig v1.4.0
 	gopkg.in/yaml.v2 v2.4.0
@@ -13,7 +14,6 @@ require (
 require (
 	github.com/Masterminds/goutils v1.1.1 // indirect
 	github.com/Masterminds/semver v1.5.0 // indirect
-	github.com/google/uuid v1.3.0 // indirect
 	github.com/huandu/xstrings v1.3.2 // indirect
 	github.com/imdario/mergo v0.3.13 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
