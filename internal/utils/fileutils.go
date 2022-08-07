fd3c5453e164d2adb0dd7b9ca74a2fef70cf475b
Krieg, Alexander <alexander.krieg@ionos.com>
2022-08-07 10:50:23 +0200

    🐛 Improve file filter

--- internal/utils/fileutils.go
+++ internal/utils/fileutils.go
@@ -14,10 +14,19 @@ func WalkMatch(root, pattern string) ([]string, error) {
 		if info.IsDir() {
 			return nil
 		}
+		info.Mode()
 		if matched, err := filepath.Match(pattern, filepath.Base(path)); err != nil {
 			return err
-		} else if matched && filepath.Dir(path) == root {
-			matches = append(matches, path)
+		} else if matched {
+			if filepath.IsAbs(root) {
+				if filepath.Dir(path) == root {
+					matches = append(matches, path)
+				} else {
+					return nil
+				}
+			} else {
+				matches = append(matches, path)
+			}
 		}
 		return nil
 	})
