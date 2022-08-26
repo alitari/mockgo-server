cec762ef7ffdd23f2baa353ec08373a811cdfbe1
Krieg, Alexander <alexander.krieg@ionos.com>
2022-08-26 15:59:01 +0200

    👷 Activate skaffold

---
+++ scripts/integration-test-precheck.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+MINIKUBE_IP=$(minikube ip)
+CONFIG_HOST=config.${MINIKUBE_IP}.nip.io
+MOCK_HOST=mock.${MINIKUBE_IP}.nip.io
+curl -v -f http://${CONFIG_HOST}/health
+curl -v -f http://${MOCK_HOST}/hello
