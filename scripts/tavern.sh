51082e119ff353a76dbc795f6b970755ce09b819
Krieg, Alexander <alexander.krieg@ionos.com>
2022-08-27 17:10:54 +0200

    👷 Publish report

--- scripts/tavern.sh
+++ scripts/tavern.sh
@@ -7,7 +7,7 @@ MINIKUBE_IP=$(minikube ip)
 CONFIG_HOST=config.${MINIKUBE_IP}.nip.io
 MOCK_HOST=mock.${MINIKUBE_IP}.nip.io
 
-docker run --network host -e CONFIG_HOST=$CONFIG_HOST -e MOCK_HOST=$MOCK_HOST -v ${PWD}/test/tavern:/tavern tavern:$TAVERNVER py.test -vv --html=/tavern/report.html --self-contained-html /tavern/test.tavern.yaml
+docker run --network host -e CONFIG_HOST=$CONFIG_HOST -e MOCK_HOST=$MOCK_HOST -v ${PWD}/test/tavern:/tavern -v ${PWD}/reports:/reports tavern:$TAVERNVER py.test -vv --html=/reports/tavern.html --self-contained-html /tavern/test.tavern.yaml
     #   - -m
     #   - pytest
     #   # Point to any global configuration files
