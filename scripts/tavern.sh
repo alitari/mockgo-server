3da8f22bf8e5aa7bd67a87be5ab162433c757e79
Alexander Krieg <alitari67@gmail.com>
2022-09-04 07:24:16 +0200

    ✅ Add hello integration tests (#22)
    * ✅ Add hello integration tests
    
    * ✅ Fix precheck test
    
    Co-authored-by: Krieg, Alexander <alexander.krieg@ionos.com>

--- scripts/tavern.sh
+++ scripts/tavern.sh
@@ -8,13 +8,3 @@ CONFIG_HOST=config.${MINIKUBE_IP}.nip.io
 MOCK_HOST=mock.${MINIKUBE_IP}.nip.io
 
 docker run --network host -e CONFIG_HOST=$CONFIG_HOST -e MOCK_HOST=$MOCK_HOST -v ${PWD}/test/tavern:/tavern -v ${PWD}/reports:/reports tavern:$TAVERNVER py.test -vv --html=/reports/tavern.html --self-contained-html /tavern/test.tavern.yaml
-    #   - -m
-    #   - pytest
-    #   # Point to any global configuration files
-    #   - --tavern-global-cfg
-    #   - /integration_tests/local_urls.yaml
-    #   # And any other flags you want to pass
-    #   - -p
-    #   - no:logging
-    #   # And then point to the folder we mounted above
-    #   - /integration_teststest.tavern.yaml
