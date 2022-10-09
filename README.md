777c6ff523220df2ef4a83accfb1aa05d631414c
Krieg, Alexander <alexander.krieg@ionos.com>
2022-10-09 11:43:39 +0200

    📦️ Setup helm repo build

---
+++ README.md
@@ -0,0 +1,9 @@
+# helm repo mockgo-server
+
+## how to create
+
+```bash
+# define tag/branch with arg
+./build.sh master
+```
+
---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+tag=$1
+
+set -e
+
+mkdir -p tmp
+git clone --depth 1 --branch "$tag" https://github.com/alitari/mockgo-server.git tmp
+cd tmp
+helm package deployments/helm/mockgo-server
+cd -
+mv tmp/mockgo-server-*.tgz .
+rm -rf tmp
+helm repo index --url https://alitari.github.io/mockgo-server .
+
+
+
+
