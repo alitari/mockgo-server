3abb8d2ee33984e26cecde14d7d2758abd282556
Krieg, Alexander <alexander.krieg@ionos.com>
2022-10-10 15:50:52 +0200

    👷 Fix multiplatform scripts

--- scripts/github-create-release.sh
+++ scripts/github-create-release.sh
@@ -13,10 +13,20 @@ PATH="$PATH:$(go env GOPATH)/bin"
 
 # create executabels
 rm -f ./bin/*
-./scripts/go-build-mockgo-standalone.sh
+for target in amd64 arm64
+do
+    ./scripts/go-build-mockgo-standalone.sh linux $target
+done
+./scripts/go-build-mockgo-standalone.sh windows amd64
+
 ./scripts/go-build-grpc-matchstore.sh
 ./scripts/go-build-grpc-kvstore.sh
-./scripts/go-build-mockgo-grpc.sh
+
+for target in amd64 arm64
+do
+./scripts/go-build-mockgo-grpc.sh linux $target
+done
+./scripts/go-build-mockgo-grpc.sh windows amd64
 
 # tgz
 for file in ./bin/*
@@ -25,11 +35,11 @@ do
 done
 
 # login in github
-gh auth login --with-token < .github/token
-gh auth status
-gh config set prompt disabled
+# gh auth login --with-token < .github/token
+# gh auth status
+# gh config set prompt disabled
 
-# create release with tgz as assets
-gh release create $releaseTag ./bin/*.tgz
+# # create release with tgz as assets
+# gh release create $releaseTag ./bin/*.tgz
 
-gh auth logout -h github.com
+# gh auth logout -h github.com
--- scripts/go-build-mockgo-standalone.sh
+++ scripts/go-build-mockgo-standalone.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-
 set -e
+
+if [ $# -lt 2 ]
+then
+    echo "Need 2 args, os and arch"
+    exit 1
+fi
+
+os=$1
+arch=$2
 
 cd mockgo-standalone/cmd
 
-for target in amd64 arm64
-do
-    CGO_ENABLED=0 GOARCH=$target GOOS=linux go build -v -o ../../bin/mockgo-standalone-linux-${target}
-done
-
-CGO_ENABLED=0 GOARCH=amd64 GOOS=windows go build -v -o ../../bin/mockgo-standalone-windows-amd64
+CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -v -o ../../bin/mockgo-standalone-${os}-${arch}
 
 echo "executable files:"
 ls -l ../../bin/mockgo-standalone*
