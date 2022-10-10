7e1668b6d1bdd55e409b03bff5564acb322cb9ad
Krieg, Alexander <alexander.krieg@ionos.com>
2022-10-10 11:12:18 +0200

    👷 Create multi platform binaries

--- scripts/go-build-mockgo-grpc.sh
+++ scripts/go-build-mockgo-grpc.sh
@@ -3,7 +3,14 @@
 set -e
 
 cd mockgo-grpc/cmd
-CGO_ENABLED=0 GOOS=linux go build -v -o ../../bin/mockgo-grpc
-echo "executable file:"
-ls -l ../../bin/mockgo-grpc
+
+for target in amd64 arm64
+do
+    CGO_ENABLED=0 GOARCH=$target GOOS=linux go build -v -o ../../bin/mockgo-grpc-linux-${target}
+done
+
+CGO_ENABLED=0 GOARCH=amd64 GOOS=windows go build -v -o ../../bin/mockgo-grpc-windows-amd64
+
+echo "executable files:"
+ls -l ../../bin/mockgo-grpc*
 cd -
--- scripts/go-build-mockgo-standalone.sh
+++ scripts/go-build-mockgo-standalone.sh
@@ -3,7 +3,14 @@
 set -e
 
 cd mockgo-standalone/cmd
-CGO_ENABLED=0 GOOS=linux go build -v -o ../../bin/mockgo-standalone
-echo "executable file:"
-ls -l ../../bin/mockgo-standalone
+
+for target in amd64 arm64
+do
+    CGO_ENABLED=0 GOARCH=$target GOOS=linux go build -v -o ../../bin/mockgo-standalone-linux-${target}
+done
+
+CGO_ENABLED=0 GOARCH=amd64 GOOS=windows go build -v -o ../../bin/mockgo-standalone-windows-amd64
+
+echo "executable files:"
+ls -l ../../bin/mockgo-standalone*
 cd -
