99d1dcc3e1a5d1515a3a3ae5f271a8ed1bd2d8e2
alitari <alitari67@gmail.com>
2022-11-01 23:08:55 +0100

    🔧 Use docker to run trivy

--- scripts/docker-build-mockgo-grpc.sh
+++ scripts/docker-build-mockgo-grpc.sh
@@ -10,9 +10,10 @@ fi
 releaseTag=$1
 push="${2:-false}"
 
-docker build -f build/docker/mockgo-grpc.Dockerfile . -t alitari/mockgo-grpc:$releaseTag --no-cache
-trivy image alitari/mockgo-grpc:$releaseTag --format sarif --severity 'CRITICAL,HIGH' --output mockgo-grpc-trivy-results.sarif
-cat mockgo-grpc-trivy-results.sarif
+docker build -f build/docker/mockgo-grpc.Dockerfile . -t alitari/mockgo-grpc:$releaseTag
+# trivy image alitari/mockgo-grpc:$releaseTag --format sarif --severity 'CRITICAL,HIGH' --output mockgo-grpc-trivy-results.sarif
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd)/reports:/reports aquasec/trivy:0.34.0 image alitari/mockgo-grpc:$releaseTag --format sarif --severity 'CRITICAL,HIGH' --output /reports/mockgo-grpc-trivy-results.sarif
+cat reports/mockgo-grpc-trivy-results.sarif
 if [ "$push" == "true" ]
 then
     docker push alitari/mockgo-grpc:$releaseTag
--- scripts/docker-build-mockgo-standalone.sh
+++ scripts/docker-build-mockgo-standalone.sh
@@ -11,7 +11,8 @@ releaseTag=$1
 push="${2:-false}"
 
 docker build -f build/docker/mockgo-standalone.Dockerfile . -t alitari/mockgo-standalone:$releaseTag --no-cache
-trivy image alitari/mockgo-standalone:$releaseTag --format sarif --severity 'CRITICAL,HIGH' --output mockgo-standalone-trivy-results.sarif
+# trivy image alitari/mockgo-standalone:$releaseTag --format sarif --severity 'CRITICAL,HIGH' --output mockgo-standalone-trivy-results.sarif
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd)/reports:/reports aquasec/trivy:0.34.0 image alitari/mockgo-standalone:$releaseTag --format sarif --severity 'CRITICAL,HIGH' --output /reports/mockgo-standalone-trivy-results.sarif
 if [ "$push" == "true" ]
 then
     docker push alitari/mockgo-standalone:$releaseTag
