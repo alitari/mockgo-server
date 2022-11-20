5927201365520e0e261099fd942fe58a6f627ef9
alitari <alitari67@gmail.com>
2022-11-20 22:37:03 +0100

    👷 Keep version path

--- scripts/go-build-mockgo.sh
+++ scripts/go-build-mockgo.sh
@@ -15,12 +15,9 @@ releaseTag=$4
 echo "building mockgo-$variant $releaseTag ..."
 
 cd "mockgo-${variant}/cmd"
-cp main.go ../..
 sed -i "s/const versionTag = .*/const versionTag = \"${releaseTag}\"/g" main.go
 
 CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -v -o ../../bin/mockgo-${variant}-${os}-${arch}
-rm main.go
-mv ../../main.go main.go
 
 echo "executable file:"
 ls -l ../../bin/mockgo-${variant}-${os}-${arch}
