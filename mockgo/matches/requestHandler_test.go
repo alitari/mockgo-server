537a2a2f41ddd17ac6de3808750c53f457314c30
alitari <alitari67@gmail.com>
2022-12-04 17:53:26 +0100

    ✅ Add error tests for match api

--- mockgo/matches/requestHandler_test.go
+++ mockgo/matches/requestHandler_test.go
@@ -1,6 +1,7 @@
 package matches
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 	"testing"
@@ -16,11 +17,38 @@ const (
 	password = "password"
 )
 
-var matchesRequestHandler *MatchesRequestHandler
+type ErrorMatchstore struct {
+}
+
+func (s *ErrorMatchstore) GetMatches(endpointId string) ([]*Match, error) {
+	return nil, fmt.Errorf("error in get matches")
+}
+func (s *ErrorMatchstore) GetMismatches() ([]*Mismatch, error) {
+	return nil, fmt.Errorf("error in get mismatches")
+}
+func (s *ErrorMatchstore) GetMatchesCount(endpointId string) (uint64, error) {
+	return 0, fmt.Errorf("error in get matches count")
+}
+func (s *ErrorMatchstore) AddMatch(endpointId string, match *Match) error {
+	return fmt.Errorf("error in add match")
+}
+func (s *ErrorMatchstore) AddMismatch(*Mismatch) error {
+	return fmt.Errorf("error in add mismatch")
+}
+func (s *ErrorMatchstore) GetMismatchesCount() (uint64, error) {
+	return 0, fmt.Errorf("error in get mismatches count")
+}
+func (s *ErrorMatchstore) DeleteMatches(endpointId string) error {
+	return fmt.Errorf("error in delete matches")
+}
+func (s *ErrorMatchstore) DeleteMismatches() error {
+	return fmt.Errorf("error in delete mismatches")
+}
+
+var matchesRequestHandler = NewMatchesRequestHandler("", username, password, NewInMemoryMatchstore(uint16(100)), logging.NewLoggerUtil(logging.Debug))
+var matchesRequestHandlerErroneous = NewMatchesRequestHandler("", username, password, &ErrorMatchstore{}, logging.NewLoggerUtil(logging.Debug))
 
 func TestMain(m *testing.M) {
-	logger := logging.NewLoggerUtil(logging.Debug)
-	matchesRequestHandler = NewMatchesRequestHandler("", username, password, NewInMemoryMatchstore(uint16(100)), logger)
 	router := mux.NewRouter()
 	matchesRequestHandler.AddRoutes(router)
 	testutil.StartServing(router)
@@ -29,14 +57,16 @@ func TestMain(m *testing.M) {
 	os.Exit(code)
 }
 
-func TestMatchesRequestHandler_health(t *testing.T) {
+func TestMatchesRequestHandler_serving_health(t *testing.T) {
 	assert.NoError(t, testutil.AssertResponseStatusOfRequestCall(t,
 		testutil.CreateOutgoingRequest(t, http.MethodGet, "/health", testutil.CreateHeader(), ""), http.StatusOK))
 }
 
-func TestMatchesRequestHandler_getMatches(t *testing.T) {
+func TestMatchesRequestHandler_serving_getMatches(t *testing.T) {
 	endpointId := "myEndpointId"
-	err := matchesRequestHandler.matchStore.AddMatch(endpointId, createMatch(endpointId))
+	err := matchesRequestHandler.matchStore.DeleteMatches(endpointId)
+	assert.NoError(t, err)
+	err = matchesRequestHandler.matchStore.AddMatch(endpointId, createMatch(endpointId))
 	assert.NoError(t, err)
 	request := testutil.CreateOutgoingRequest(t, http.MethodGet, "/matches/"+endpointId,
 		testutil.CreateHeader().WithAuth(username, password).WithJsonAccept(), "")
@@ -44,91 +74,121 @@ func TestMatchesRequestHandler_getMatches(t *testing.T) {
 		assert.Equal(t, http.StatusOK, response.StatusCode)
 		assert.Equal(t, `[{"endpointId":"myEndpointId","timestamp":"2009-11-17T20:34:58.651387237Z","actualRequest":{"method":"GET","url":"./http://myhost","header":null,"host":""},"actualResponse":null}]`, responseBody)
 	})
-
 }
 
-func TestMatchesRequestHandler_getMismatches(t *testing.T) {
-
-}
-func TestMatchesRequestHandler_addMatches(t *testing.T) {
-
-}
-func TestMatchesRequestHandler_addMismatches(t *testing.T) {
-
-}
-func TestMatchesRequestHandler_deleteMatches(t *testing.T) {
-
-}
-func TestMatchesRequestHandler_deleteMismatches(t *testing.T) {
-
+func TestMatchesRequestHandler_getMatches_Error(t *testing.T) {
+	request := testutil.CreateIncomingRequest(http.MethodGet, "/matches", testutil.CreateHeader(), "")
+	assert.NoError(t, testutil.AssertHandlerFunc(t, request, matchesRequestHandlerErroneous.handleGetMatches, func(response *http.Response, responseBody string) {
+		assert.Equal(t, http.StatusInternalServerError, response.StatusCode)
+		assert.Equal(t, "error in get matches\n", responseBody)
+	},
+	))
 }
 
-// func TestKVStoreRequestHandler_setKVStore(t *testing.T) {
-// 	err := kvstoreHandler.kvstore.PutAll(map[string]interface{}{})
-// 	assert.NoError(t, err)
-// 	key := randString(5)
-// 	util.RequestCall(t, httpClient, http.MethodPut, urlPrefix+"/kvstore/"+key,
-// 		map[string][]string{headers.Authorization: {util.BasicAuth(username, password)}, headers.ContentType: {"application/json"}},
-// 		`{ "testkey":"testvalue"}`, http.StatusNoContent, nil)
-// 	val, err := kvstoreHandler.kvstore.Get(key)
-// 	assert.NoError(t, err)
-// 	assert.Equal(t, map[string]interface{}{"testkey": "testvalue"}, val)
-// }
+func TestMatchesRequestHandler_serving_getMatchesCount(t *testing.T) {
+	endpointId := "myEndpointId"
+	err := matchesRequestHandler.matchStore.DeleteMatches(endpointId)
+	assert.NoError(t, err)
+	err = matchesRequestHandler.matchStore.AddMatch(endpointId, createMatch(endpointId))
+	assert.NoError(t, err)
+	request := testutil.CreateOutgoingRequest(t, http.MethodGet, "/matchesCount/"+endpointId,
+		testutil.CreateHeader().WithAuth(username, password).WithJsonAccept(), "")
+	testutil.AssertResponseOfRequestCall(t, request, func(response *http.Response, responseBody string) {
+		assert.Equal(t, http.StatusOK, response.StatusCode)
+		assert.Equal(t, `1`, responseBody)
+	})
+}
 
-// func TestKVStoreRequestHandler_getKVStore(t *testing.T) {
-// 	key := randString(5)
-// 	err := kvstoreHandler.kvstore.PutAll(map[string]interface{}{key: "expectedVal", "key2": "val2 not expected"})
-// 	assert.NoError(t, err)
-// 	util.RequestCall(t, httpClient, http.MethodGet, urlPrefix+"/kvstore/"+key,
-// 		map[string][]string{headers.Authorization: {util.BasicAuth(username, password)}, headers.Accept: {"application/json"}},
-// 		"", http.StatusOK,
-// 		func(responseBody string) {
-// 			assert.Equal(t, `"expectedVal"`, responseBody)
-// 		})
-// }
+func TestMatchesRequestHandler_getMatchesCount_Error(t *testing.T) {
+	request := testutil.CreateIncomingRequest(http.MethodGet, "/matchesCount", testutil.CreateHeader(), "")
+	assert.NoError(t, testutil.AssertHandlerFunc(t, request, matchesRequestHandlerErroneous.handleGetMatchesCount, func(response *http.Response, responseBody string) {
+		assert.Equal(t, http.StatusInternalServerError, response.StatusCode)
+		assert.Equal(t, "error in get matches count\n", responseBody)
+	},
+	))
+}
 
-// func TestKVStoreRequestHandler_addKVStore(t *testing.T) {
-// 	err := kvstoreHandler.kvstore.PutAll(map[string]interface{}{})
-// 	assert.NoError(t, err)
-// 	key := randString(5)
-// 	util.RequestCall(t, httpClient, http.MethodPost, urlPrefix+"/kvstore/"+key+"/add",
-// 		map[string][]string{headers.Authorization: {util.BasicAuth(username, password)}, headers.ContentType: {"application/json"}},
-// 		`{ "path": "/testpath", "value": "testvalue" }`, http.StatusNoContent, nil)
-// 	val, err := kvstoreHandler.kvstore.Get(key)
-// 	assert.NoError(t, err)
-// 	assert.Equal(t, map[string]interface{}{"testpath": "testvalue"}, val)
-// }
+func TestMatchesRequestHandler_serving_getMismatches(t *testing.T) {
+	err := matchesRequestHandler.matchStore.DeleteMismatches()
+	assert.NoError(t, err)
+	err = matchesRequestHandler.matchStore.AddMismatch(createMismatch())
+	assert.NoError(t, err)
+	request := testutil.CreateOutgoingRequest(t, http.MethodGet, "/mismatches",
+		testutil.CreateHeader().WithAuth(username, password).WithJsonAccept(), "")
+	testutil.AssertResponseOfRequestCall(t, request, func(response *http.Response, responseBody string) {
+		assert.Equal(t, http.StatusOK, response.StatusCode)
+		assert.Equal(t, `[{"MismatchDetails":"","timestamp":"2009-11-17T20:34:58.651387237Z","actualRequest":{"method":"GET","url":"./http://myhost","header":null,"host":""}}]`, responseBody)
+	})
+}
 
-// func TestKVStoreRequestHandler_removeKVStore(t *testing.T) {
-// 	key := randString(5)
-// 	err := kvstoreHandler.kvstore.PutAll(map[string]interface{}{key: map[string]string{"deletepath": "deletzevalue"}})
-// 	assert.NoError(t, err)
-// 	util.RequestCall(t, httpClient, http.MethodPost, urlPrefix+"/kvstore/"+key+"/remove",
-// 		map[string][]string{headers.Authorization: {util.BasicAuth(username, password)}, headers.ContentType: {"application/json"}},
-// 		`{ "path": "/deletepath"}`, http.StatusNoContent, nil)
-// 	all, err := kvstoreHandler.kvstore.GetAll()
-// 	assert.NoError(t, err)
-// 	assert.Equal(t, map[string]interface{}{key: map[string]interface{}{}}, all)
-// }
+func TestMatchesRequestHandler_getMisMatches_Error(t *testing.T) {
+	request := testutil.CreateIncomingRequest(http.MethodGet, "/mismatches", testutil.CreateHeader(), "")
+	assert.NoError(t, testutil.AssertHandlerFunc(t, request, matchesRequestHandlerErroneous.handleGetMismatches, func(response *http.Response, responseBody string) {
+		assert.Equal(t, http.StatusInternalServerError, response.StatusCode)
+		assert.Equal(t, "error in get mismatches\n", responseBody)
+	},
+	))
+}
 
-// func TestKVStoreRequestHandler_uploadKVStore(t *testing.T) {
-// 	err := kvstoreHandler.kvstore.PutAll(map[string]interface{}{})
-// 	assert.NoError(t, err)
-// 	util.RequestCall(t, httpClient, http.MethodPut, urlPrefix+"/kvstore",
-// 		map[string][]string{headers.Authorization: {util.BasicAuth(username, password)}, headers.ContentType: {"application/json"}},
-// 		`{"store1":"store1value","store2":{"key2":"value2"}}`, http.StatusNoContent, nil)
-// 	all, err := kvstoreHandler.kvstore.GetAll()
-// 	assert.NoError(t, err)
-// 	assert.Equal(t, map[string]interface{}{"store1": "store1value", "store2": map[string]interface{}{"key2": "value2"}}, all)
-// }
+func TestMatchesRequestHandler_serving_getMismatchesCount(t *testing.T) {
+	err := matchesRequestHandler.matchStore.DeleteMismatches()
+	assert.NoError(t, err)
+	err = matchesRequestHandler.matchStore.AddMismatch(createMismatch())
+	assert.NoError(t, err)
+	request := testutil.CreateOutgoingRequest(t, http.MethodGet, "/mismatchesCount",
+		testutil.CreateHeader().WithAuth(username, password).WithJsonAccept(), "")
+	testutil.AssertResponseOfRequestCall(t, request, func(response *http.Response, responseBody string) {
+		assert.Equal(t, http.StatusOK, response.StatusCode)
+		assert.Equal(t, `1`, responseBody)
+	})
+}
 
-// func TestKVStoreRequestHandler_downloadKVStore(t *testing.T) {
-// 	err := kvstoreHandler.kvstore.PutAll(map[string]interface{}{"store1": "store1value", "store2": map[string]interface{}{"key2": "value2"}})
-// 	assert.NoError(t, err)
-// 	util.RequestCall(t, httpClient, http.MethodGet, urlPrefix+"/kvstore",
-// 		map[string][]string{headers.Authorization: {util.BasicAuth(username, password)}, headers.Accept: {"application/json"}},
-// 		"", http.StatusOK,
-// 		func(responseBody string) {
-// 			assert.Equal(t, `{"store1":"store1value","store2":{"key2":"value2"}}`, responseBody)
-// 		})
-// }
+func TestMatchesRequestHandler_getMismatchesCount_Error(t *testing.T) {
+	request := testutil.CreateIncomingRequest(http.MethodGet, "/mismatchesCount", testutil.CreateHeader(), "")
+	assert.NoError(t, testutil.AssertHandlerFunc(t, request, matchesRequestHandlerErroneous.handleGetMismatchesCount, func(response *http.Response, responseBody string) {
+		assert.Equal(t, http.StatusInternalServerError, response.StatusCode)
+		assert.Equal(t, "error in get mismatches count\n", responseBody)
+	},
+	))
+}
+
+func TestMatchesRequestHandler_serving_deleteMatches(t *testing.T) {
+	endpointId := "myEndpointId"
+	err := matchesRequestHandler.matchStore.AddMatch(endpointId, createMatch(endpointId))
+	assert.NoError(t, err)
+	request := testutil.CreateOutgoingRequest(t, http.MethodDelete, "/matches/"+endpointId,
+		testutil.CreateHeader().WithAuth(username, password).WithJsonAccept(), "")
+	testutil.AssertResponseStatusOfRequestCall(t, request, http.StatusOK)
+	matches, err := matchesRequestHandler.matchStore.GetMatches(endpointId)
+	assert.NoError(t, err)
+	assert.Empty(t, matches)
+}
+
+func TestMatchesRequestHandler_deleteMatches_Error(t *testing.T) {
+	request := testutil.CreateIncomingRequest(http.MethodDelete, "/matches", testutil.CreateHeader(), "")
+	assert.NoError(t, testutil.AssertHandlerFunc(t, request, matchesRequestHandlerErroneous.handleDeleteMatches, func(response *http.Response, responseBody string) {
+		assert.Equal(t, http.StatusInternalServerError, response.StatusCode)
+		assert.Equal(t, "error in delete matches\n", responseBody)
+	},
+	))
+}
+
+func TestMatchesRequestHandler_serving_deleteMismatches(t *testing.T) {
+	err := matchesRequestHandler.matchStore.AddMismatch(createMismatch())
+	assert.NoError(t, err)
+	request := testutil.CreateOutgoingRequest(t, http.MethodDelete, "/mismatches",
+		testutil.CreateHeader().WithAuth(username, password).WithJsonAccept(), "")
+	testutil.AssertResponseStatusOfRequestCall(t, request, http.StatusOK)
+	mismatches, err := matchesRequestHandler.matchStore.GetMismatches()
+	assert.NoError(t, err)
+	assert.Empty(t, mismatches)
+}
+
+func TestMatchesRequestHandler_deleteMismatches_Error(t *testing.T) {
+	request := testutil.CreateIncomingRequest(http.MethodGet, "/mismatches", testutil.CreateHeader(), "")
+	assert.NoError(t, testutil.AssertHandlerFunc(t, request, matchesRequestHandlerErroneous.handleDeleteMismatches, func(response *http.Response, responseBody string) {
+		assert.Equal(t, http.StatusInternalServerError, response.StatusCode)
+		assert.Equal(t, "error in delete mismatches\n", responseBody)
+	},
+	))
+}
