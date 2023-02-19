9d27e03680c09d05d7b54cdfb8de7840c4d4fa9b
alitari <alitari67@gmail.com>
2023-02-19 20:37:47 +0100

    Remove old scripts

--- scripts/go-run-standalone.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd mockgo-standalone/cmd
-MOCK_DIR="../../test/main" RESPONSE_DIR="../../test/main/responses" go run . 
-cd -
--- test/tavern/tavern.Dockerfile
+++
@@ -1,6 +0,0 @@
-# tavern.Dockerfile
-FROM python:3.10-slim
-
-ARG TAVERNVER
-RUN pip3 install tavern==$TAVERNVER pytest-html
-
